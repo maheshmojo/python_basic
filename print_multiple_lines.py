cea15fc5dd74120fb7ee4afbc5ad54c4019f030f
maheshmojo <maheshmac21@gmail.com>
2022-09-29 16:10:04 +0000

    playing with print function

--- print_multiple_lines.py
+++ print_multiple_lines.py
@@ -1,4 +1,13 @@
 print ("hello future python programmer");\
 print ("hello future python programmer");\
-print ("hello future python programmer")
+print ("hello future python programmer");\
+
+print ("hello, \nfuture python programmer")
+
+print ("hello", end=" ");\
+print ("future python programmer")
+
+print ("Hello", "Welcome to see you",sep=":-)")
+
+print ("So", "enjoy Python?", sep=",", end=":-)" )
 
