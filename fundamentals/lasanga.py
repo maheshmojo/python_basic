da0d1c06fd76762529a795317603a32baec35f0a
maheshmojo <maheshmac21@gmail.com>
2022-12-08 06:07:02 +0000

    Added String functions and how to pass variable to print statement

---
+++ fundamentals/lasanga.py
@@ -0,0 +1,6 @@
+import lasanga
+
+lasa = lasanga.EXPECTED_BAKE_TIME
+
+print (lasa)
+
---
+++ strings.py
@@ -0,0 +1,4 @@
+string1 = "this ain't good"
+
+print (string1)
+print (f"having a lot {string1}?") #best way to pass variables in print statement
