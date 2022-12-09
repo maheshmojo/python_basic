96477ff85d90c756a7d79cb6f90105b2fb30b408
maheshmojo <maheshmac21@gmail.com>
2022-12-09 08:25:48 +0000

    added scerets

--- strings.py
+++ strings.py
@@ -1,4 +1,4 @@
 string1 = "this ain't good"
-string2 = "secrets.TITILE" 
+string2 = "${secrets.TITILE}" 
 print (string1)
 print (f"having a lot {string1}?",f"Just {string2}") #best way to pass variables in print statement 
